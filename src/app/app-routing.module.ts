074b1343f19c11459f52d46bb6da950af66871b8
unknown <ArtyomSnow>
2022-03-22 01:08:40 -0300

    v2

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+
+import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { HomeComponent } from './components/home/home.component';
+import { Pagina404Component } from './components/pagina404/pagina404.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+
+  {
+    path:'home', redirectTo: ''
+  },
+
+  {
+    path: 'contacto', component: ContactoComponent
+  },
+
+  {
+    path: '404', component: Pagina404Component
+  },
+
+  {
+    path: '**', redirectTo: '404'
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
